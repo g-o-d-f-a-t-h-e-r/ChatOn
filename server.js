3434922e8a67fb8741902f36867e62833018ab2c
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2020-11-16 22:10:42 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,114 @@
+//IMPORTS
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socket = require('socket.io')
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+// const users = require('./utils/users')
+
+
+//SETUP'S
+const app = express()
+const server = http.createServer(app)
+const io = socket(server)
+port = 80 || process.env.port
+
+
+//EXPRESS STUFF
+app.use('/static', express.static('static'))
+
+//PUG STUFF
+app.set('view engine', 'pug')  //Set the template engine as pug
+app.set('views', path.join(__dirname, 'views'))  //Set the views directory.
+
+
+
+
+//ENDPOINTS
+app.get('/', (req, res) => {
+    res.status(200).render("index.pug")
+})
+
+app.get('/login', (req, res) => {
+    res.status(200).render("login.pug")
+})
+
+
+app.get('/chat', (req, res) => {
+    res.status(200).render("chat.pug")
+})
+
+
+
+
+//START THE SERVER
+server.listen(port, () => {
+    console.log(`The server is running in port: ${port}`)
+})
+
+
+
+//SOCKET.IO CONNECTIONS
+io.on('connection', socket => {
+
+    socket.on('joinRoom', ({ username, room }) => {
+
+        //JOINING THE ROOM
+        const user = userJoin(socket.id, username, room)
+        socket.join(user.room)
+
+        //WELCOME CURRENT USER 
+        socket.emit('message', formatMessage(``, `Hello <span style = "color: rgb(120, 0, 145); font-weight: bold; text-transform: uppercase">${user.username} !</span> Welcome to ChatOn ! ''`))
+
+        //BROADCAST WHEN A USER CONNECTS 
+        socket.broadcast.to(user.room).emit('message', formatMessage(user.username, ' joined the Chat'))
+
+        // USERS AND ROOM INFO
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
+
+    //LISTEN FOR CHAT MESSAGE
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id)
+        socket.emit('sentMessage', formatMessage(user.username, msg))
+        socket.broadcast.to(user.room).emit('chatMessage', formatMessage(user.username, msg))
+    })
+
+    //BROADCAST WHEN A USER LEAVES
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(user.username, ' left the Chat'))
+        }
+
+
+        // USERS AND ROOM INFO
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ static/client.js
@@ -0,0 +1,132 @@
+
+
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chatContainer')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('userList')
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
+
+
+//ADDING AUDIO  
+const audio = new Audio('/static/notification.mp3')
+const incomming = new Audio('/static/incomming.mp3')
+const sent = new Audio('/static/sent.mp3')
+
+
+//GET USERNAME AND ROOM FROM URL
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+
+})  
+
+const socket = io('http://localhost')
+
+
+//JOIN CHAT ROOM
+socket.emit('joinRoom', {username, room})
+
+
+
+// GET ROOM AND USERS
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room)
+    outputUsers(users)
+})
+
+ 
+//LISTENING FROM THE SERVER
+socket.on('message', message =>{
+    console.log(message)
+    incomming.pause()
+    incomming.currentTime = 0
+    incomming.play()
+    outputMessage(message, 'center')
+    //SCROLL DOWN
+    chatMessages.scrollTop = chatMessages.scrollHeight
+})
+
+socket.on('chatMessage', msg => {
+    console.log(msg)
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play()
+    outputMessage(msg, 'left')
+    //SCROLL DOWN
+    chatMessages.scrollTop = chatMessages.scrollHeight 
+    
+})
+
+socket.on('sentMessage', msg => {
+    
+    sent.pause()
+    sent.currentTime = 0
+    sent.play()
+    outputMessage(msg, 'right')
+    
+
+    //SCROLL DOWN
+    chatMessages.scrollTop = chatMessages.scrollHeight
+})
+
+
+
+
+//EVENT LISTNER FOR MESSAGE SEND
+chatForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    //Get message Text
+    const msg = e.target.elements.msg.value
+
+    //Emitting it to the server
+    socket.emit('chatMessage',msg)
+
+    
+    //CLEAR INPUT
+    e.target.elements.msg.value = ''
+    e.target.elements.msg.focus()
+
+  
+})
+
+
+
+//OUTPUT MESSAGE TO DOM
+function outputMessage(msg, position){
+    const div = document.createElement('div')
+    div.classList.add(position)
+    if(position == 'center'){
+        div.innerHTML = `<p class="text"> '${msg.username}' ${msg.text}</p>`
+    }
+    else if(position == 'left'){
+        div.innerHTML = `<p class="meta">${msg.username} : <span>${msg.time}</span> </p>
+        <p class="text"> ${msg.text}</p> ` 
+        audio.play() 
+    }
+    else{
+        div.innerHTML = `<p class="meta">You : <span>${msg.time}</span> </p>
+        <p class="text"> ${msg.text}</p> `
+    }
+    
+    chatMessages.appendChild(div)
+
+}
+
+// ADD ROOM NAME TO DOM
+function outputRoomName(room){
+    roomName.innerText = room
+}
+
+
+// ADD USERS TO THE DOM 
+function outputUsers(users){
+    userList.innerHTML = `
+        ${users.map(user => `<li class='name'><img id="img" src="../static/img/online_icon.png" alt="online"><p id='username'>${user.username}</p></li>`).join('')}`
+}
+
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('open')
+})
