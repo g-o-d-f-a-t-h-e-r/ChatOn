fdbfbc5ce6d7772c20e894823c0656a52e8c47e9
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2020-11-18 18:08:08 +0530

    make it better

--- utils/messages.js
+++ utils/messages.js
@@ -4,8 +4,8 @@ const moment = require('moment')
 function formatMessage(username, text){
     return{
         username,
-        text,
-        time: moment().utc('h:mm A').format('LLL')
+        text
+        // time: moment().utc('h:mm A').format('LLL')
     }
 }
 
