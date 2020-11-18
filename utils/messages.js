739e7cc93ce528660850a6493f6828b6ad8941d5
g-o-d-f-a-t-h-e-r <chetandange100699@gmail.com>
2020-11-18 18:15:19 +0530

    make it better

--- utils/messages.js
+++ utils/messages.js
@@ -4,8 +4,11 @@ const moment = require('moment')
 function formatMessage(username, text){
     return{
         username,
-        text
+        text,
         // time: moment().utc('h:mm A').format('LLL')
+        time: moment.tz.link("Asia/Calcutta|Asia/Kolkata").format('h:mm A')
+        
+
     }
 }
 
